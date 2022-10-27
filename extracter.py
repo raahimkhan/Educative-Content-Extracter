d9ac1a15636923673454682dc195f304db96ce47
raahimkhan <21100157@lums.edu.pk>
2022-10-28 00:51:15 +0500

    Added some more features

--- extracter.py
+++ extracter.py
@@ -6,6 +6,8 @@ from progressbar import ProgressBar
 import time
 import os
 import os.path
+from os import system
+import re
 
 def startChrome():
     options = webdriver.ChromeOptions()
@@ -80,8 +82,8 @@ def main():
     myProgressBar(3)
     print('')
     driver = startChrome()
-    goToEducativeMainPage(driver)
     print('')
+    goToEducativeMainPage(driver)
     input("Please log in using your account and then press enter to continue.")
     print('')
     print('Processing. Please wait...')
@@ -90,52 +92,195 @@ def main():
     print('')
     acceptedURLs = ["https://www.educative.io/learn", "https://www.educative.io/explore", "https://www.educative.io/learning-plans", "https://www.educative.io/projects", "https://www.educative.io/paths", "https://www.educative.io/assessments", "https://www.educative.io/onboarding/dashboard", "https://www.educative.io/onboarding/plans", "https://www.educative.io/onboarding/drafts", "https://www.educative.io/onboarding/modules"]
     if driver.current_url not in acceptedURLs:
+        system('clear')
         print("We detected you are not logged in yet! Please login and then try again.")
         driver.quit()
     else:
-        print("Please enter URL of the **FIRST** lesson of the course you want to scrape. Make sure the link is of the **PUBLISHED** version of the course and not the page editor.")
-        print("For example: https://www.educative.io/courses/react-beginner-to-advanced/JY1xJkJgrqg")
+        system('clear')
+        print('Press 1 to extract content from all lessons of the course (quiz, project, mini-project, and assessment lessons will be skipped)')
         print('')
-        firstURL = input("Enter URL here: ")
+        print('press 2 to extract lessons within a range (quiz, project, mini-project, and assessment lessons will be skipped)')
         print('')
-        linksExtracted = False
-        try:
-            driver.get(firstURL)
-            myProgressBar(5)
-            print('')
-            print('Extracting course lesson links. Please wait...')
-            print('')
-            extractLessonLinks(driver)
-            linksExtracted = True
-        except:
-            print('Some error occured while fetching course lesson links. Please try later. Content scrapping failed.')
-            driver.quit()
-        if linksExtracted == True:
-            resp = getLinksFromFile()
-            if resp[0] == False:
-                print('Links.txt file is empty. Please make sure there are lesson links inside the file. Content scrapping failed.')
-                driver.quit()
+        while True:
+            choice = input("Enter your choice: ")
+            if choice.isdigit() == False:
+                print('Invalid choice! Enter again.')
+                print('')
             else:
-                links = resp[1]
-                count = 0
-                try:
-                    print('Content scrapping started. Please be patient.')
-                    print('You can open the course.txt file in any editor to monitor its contents side by side.')
+                if int(choice) not in [1, 2]:
+                    print('Invalid choice! Enter again.')
                     print('')
-                    for link in links:
-                        driver.get(link)
-                        myProgressBar(5)
-                        getH1Tags(driver)
-                        getPTags(driver)
-                        getliTags(driver)
-                        print('Lesson: ', link, ' content added to course.txt file. Total lessons done = ', count + 1)
+                else:
+                    break
+        if int(choice) == 1:
+            system('clear')
+            print("Please enter URL of any lesson of the course you want to scrape. Make sure the URL is of the **PUBLISHED** version of the course and not the page editor.")
+            print("For example: https://www.educative.io/courses/react-beginner-to-advanced/JY1xJkJgrqg")
+            print('')
+            while True:
+                firstURL = input("Enter URL here: ")
+                regex = re.compile('^https://www.educative.io/courses/')
+                if re.match(regex, firstURL):
+                    if len(firstURL.split("//")[1].split("/")) != 4:
+                        print('URL does not seem to be from the lesson of an Educative course. Please enter again.')
                         print('')
-                        count = count + 1
+                    else:
+                        break
+                else:
+                    print('URL does not seem to be from the lesson of an Educative course. Please enter again.')
                     print('')
-                    print('All done. Content scrapped from a total of ', count, ' lessons.')
+            print('')
+            linksExtracted = False
+            try:
+                driver.get(firstURL)
+                myProgressBar(5)
+                print('')
+                print('Extracting course lesson links. Please wait...')
+                print('')
+                extractLessonLinks(driver)
+                linksExtracted = True
+            except:
+                system('clear')
+                print('Some error occured while fetching course lesson links. Please try later. Content scrapping failed.')
+                driver.quit()
+            if linksExtracted == True:
+                resp = getLinksFromFile()
+                if resp[0] == False:
+                    system('clear')
+                    print('Links.txt file is empty. Please make sure there are lesson links inside the file. Content scrapping failed.')
                     driver.quit()
-                except:
-                    print('Sorry! Something went wrong. Please try again later.')
+                else:
+                    links = resp[1]
+                    os.remove("links.txt")
+                    count = 0
+                    try:
+                        system('clear')
+                        print('Content scrapping started. Please be patient.')
+                        print('You can open the course.txt file in any editor to monitor its contents side by side.')
+                        print('')
+                        for link in links:
+                            driver.get(link)
+                            myProgressBar(5)
+                            getH1Tags(driver)
+                            getPTags(driver)
+                            getliTags(driver)
+                            print('Lesson: ', link, ' content added to course.txt file. Total lessons done = ', count + 1)
+                            print('')
+                            count = count + 1
+                        system('clear')
+                        print('All done. Content scrapped from a total of ', count, ' lessons.')
+                        print('Open the course.txt file to see the results.')
+                        driver.quit()
+                    except:
+                        system('clear')
+                        print('Sorry! Something went wrong. Please try again later.')
+                        driver.quit()
+        else:
+            system('clear')
+            print("Please enter the URL of the lesson from where you want to start content scrapping.")
+            print("Make sure the URL is of the **PUBLISHED** version of the course and not the page editor.")
+            print("For example: https://www.educative.io/courses/react-beginner-to-advanced/JY1xJkJgrqg")
+            print('')
+            firstURL = None
+            while True:
+                firstURL = input("Enter URL here: ")
+                regex = re.compile('^https://www.educative.io/courses/')
+                if re.match(regex, firstURL):
+                    if len(firstURL.split("//")[1].split("/")) != 4:
+                        print('URL does not seem to be from the lesson of an Educative course. Please enter again.')
+                        print('')
+                    else:
+                        break
+                else:
+                    print('URL does not seem to be from the lesson of an Educative course. Please enter again.')
+                    print('')
+
+            system('clear')
+
+            print("Please enter the URL of the lesson from where you want to stop content scrapping.")
+            print("Note that this lesson will be included in content scrapping")
+            print("Make sure the URL is of the **PUBLISHED** version of the course and not the page editor.")
+            print("For example: https://www.educative.io/courses/react-beginner-to-advanced/JPD8EKAxAqg")
+            print('')
+            secondURL = None
+            while True:
+                secondURL = input("Enter URL here: ")
+                regex = re.compile('^https://www.educative.io/courses/')
+                if re.match(regex, secondURL):
+                    if len(secondURL.split("//")[1].split("/")) != 4:
+                        print('URL does not seem to be from the lesson of an Educative course. Please enter again.')
+                        print('')
+                    else:
+                        if secondURL.split("//")[1].split("/")[2] != firstURL.split("//")[1].split("/")[2]:
+                            print("URL does not seem to be from the same course as the URL entered previously. Please enter again.")
+                            print('')
+                        else:
+                            break
+                else:
+                    print('URL does not seem to be from the lesson of an Educative course. Please enter again.')
+                    print('')
+
+            print('')
+            linksExtracted = False
+            try:
+                driver.get(firstURL)
+                myProgressBar(5)
+                print('')
+                print('Checking order of both URLs. Checking if second URL comes after the first one or not.')
+                print('')
+                extractLessonLinks(driver)
+                linksExtracted = True
+            except:
+                system('clear')
+                print('Some error occured while fetching course lesson links. Please try later. Content scrapping failed.')
+                driver.quit()
+
+            if linksExtracted == True:
+                resp = getLinksFromFile()
+                if resp[0] == False:
+                    system('clear')
+                    print('Links.txt file is empty. Please make sure there are lesson links inside the file. Content scrapping failed.')
                     driver.quit()
+                else:
+                    links = resp[1]
+                    os.remove("links.txt")
+                    # checking order of both URLs
+                    pos1 = None
+                    pos2 = None
+                    for i in range(len(links)):
+                        if links[i] == firstURL:
+                            pos1 = i
+                        if links[i] == secondURL:
+                            pos2 = i
+                    if pos2 < pos1:
+                        system('clear')
+                        print('Order of both URLs entered is not correct. Make sure the second URL is of the lesson that comes **AFTER** the first URL.')
+                        print('Try again later')
+                        driver.quit()
+                    else:
+                        count = 0
+                        links = links[pos1:(pos2+1)]
+                        try:
+                            system('clear')
+                            print('Content scrapping started. Please be patient.')
+                            print('You can open the course.txt file in any editor to monitor its contents side by side.')
+                            print('')
+                            for link in links:
+                                driver.get(link)
+                                myProgressBar(5)
+                                getH1Tags(driver)
+                                getPTags(driver)
+                                getliTags(driver)
+                                print('Lesson: ', link, ' content added to course.txt file. Total lessons done = ', count + 1)
+                                print('')
+                                count = count + 1
+                            system('clear')
+                            print('All done. Content scrapped from a total of ', count, ' lessons.')
+                            print('Open the course.txt file to see the results.')
+                            driver.quit()
+                        except:
+                            system('clear')
+                            print('Sorry! Something went wrong. Please try again later.')
+                            driver.quit()
 
 main()
